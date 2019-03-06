83e2d06ccc4b37b36684b91618c7732e203e593d
sam.chu <sam.chu@csnt.com.tw>
2019-03-06 14:41:04 +0800

    first commit

---
+++ src/main/java/com/sam/nexushelp/NexusHelpApplication.java
@@ -0,0 +1,13 @@
+package com.sam.nexushelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusHelpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexusHelpApplication.class, args);
+    }
+
+}
